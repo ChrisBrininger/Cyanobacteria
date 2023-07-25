7acebd5444c934d1279839d30eeccdd077cab791
ChrisBrininger <128190615+ChrisBrininger@users.noreply.github.com>
2023-07-25 14:23:08 -0600

    Update GeitVelocity.m

--- GeitVelocity.m
+++ GeitVelocity.m
@@ -1,36 +1,46 @@
 %% Geit Velocity
+%% Auto-Seg %%
+
 clearvars
 clc
-% % Options % %
-initialFrame = 1;
-finalFrame = 3;
-File =  BioformatsImage ( fullfile('E:\Movies\20210418_Ana_-N_-Buffer_+CaCO3\channelbf,cy5,cfp,rfp_seq0000_0000.nd2'));
-Time = 5; %(seconds)
-pixelSize = 0.324; %(Microns)
+
+% % Optons % %
+
+initFrame = 1;
+fFrame = 5;     %# of frames
+File = BioformatsImage(fullfile('E:\Movies\20210418_Ana_-N_-Buffer_+CaCO3\channelbf,cy5,cfp,rfp_seq0000_0000.nd2')); %File location here
+timeBtwnPhoto = 5;  %in seconds (get this from metadata!!!)
+pixelSize = 0.324; %microns (get this from metadata!!!)
+%remember, this is now in microns/sec!
+
 % % Script % %
-pFrame = (finalFrame - initialFrame) + 1;
-Points = NaN(pFrame,2);
-pFrame = pFrame - 1;
-Velocity = NaN(pFrame,1);
-for iFrame = (initialFrame):(finalFrame)
-    Cells = getPlane(File,1,2,iFrame);
-    imshow (Cells,[])
-%     roi = drawrectangle('StripeColor','y');
-%     rect = [roi.Vertices(1,1),roi.Vertices(1,2),(roi.Vertices(3,1)-roi.Vertices(1,1)),(roi.Vertices(2,2)-roi.Vertices(1,2))];
-%
-%     currROI = imcrop(Cells,rect);
-%
-%     imshow(currROI,[]);
-%
+
+pFrame = fFrame - initFrame + 1;
+points = NaN(pFrame, 2); %creates a pFrame x 2 vector (really an array but whos asking)
+velocity = NaN(1,pFrame-1);
+velIter = 1;
+for iFrame = (initFrame):(fFrame)
+    
+    close all
+    
+    disp(iFrame)
+    Cells = getPlane(File,1,1,iFrame);
+    imshow(Cells,[])
+    colormap('parula');
     f = drawpoint;
-    Points(iFrame,1) = f.Position(1,1);
-    Points(iFrame,2) = f.Position(1,2);
-    if iFrame == initialFrame;
-    else
-        dx = Points(iFrame,1) - Points(iFrame-1,1);
-        dy = Points(iFrame,2) - Points(iFrame-1,2);
-        H = sqrt(dx.^2 + dy.^2);
-        H = H * pixelSize;
-        Velocity(iFrame-1,1) = H/Time;
+    %need to consider pre-allocation, or switching to a lang that is more
+    %flexible...
+    points(iFrame,1) = f.Position(1,1);
+    points(iFrame,2) = f.Position(1,2);
+    if iFrame ~= initFrame
+        %Calculate velocity
+        %find dist btwn two points using pythag
+        dx = points(iFrame,1) - points(iFrame-1,1);
+        dy = points(iFrame,2) - points(iFrame-1,2);
+        dist = sqrt((dx.^2) + (dy.^2));
+        dist = dist * pixelSize;
+        velocity(velIter) = dist/timeBtwnPhoto;
+        velIter = velIter + 1;
     end
 end
+
